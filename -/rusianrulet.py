7880dee62d893434fbf0457e651bd7430ca5b6f8
hellotuba <88927076+hellotuba@users.noreply.github.com>
2025-06-25 08:37:41 +0200

    Delete - directory

--- -/rusianrulet.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-live = random.randint(1, 6)
-bullet = random.randint(1, 6)
-if live == bullet:
-    print("ŽÍVÁ! bye bye system.")
-    os.remove("C:\Windows\System32")
-else:
-    print("Systém přežil!")
